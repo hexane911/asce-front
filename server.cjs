9ebf2f1ceae6eed612b6d311db81e5ae5eb7b7da
hexane911 <koryaminmih@gmail.com>
2024-02-06 12:25:34 +0300

    removed log

--- server.cjs
+++ server.cjs
@@ -1,8 +1,6 @@
 const express = require('express')
 const path = require('path')
 
-console.log("!!!")
-
 const app = express()
 app.use(express.static(path.join(__dirname, '/dist')))
 app.get('*/*.js', (req, res) => {
