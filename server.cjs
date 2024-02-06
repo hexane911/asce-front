6d8ee359919c4200f51f9ab2bf9c3efe2d53da6e
hexane911 <koryaminmih@gmail.com>
2024-02-06 12:33:34 +0300

    server port fix

--- server.cjs
+++ server.cjs
@@ -23,6 +23,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
-const port = 4000
+const port = 3000
 app.listen(port)
 console.log('App is listening on port ' + port)
